78646a0abfc32069e5a24cadb457917b0ae0d045
Sara Hashem <hashems@oregonstate.edu>
2017-02-18 20:27:21 -0500

    Implement basic linear, grid, and relative layouts

---
+++ app/src/main/java/hashems/mobile_development_projects/ListViewHActivity.java
@@ -0,0 +1,14 @@
+package hashems.mobile_development_projects;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class ListViewHActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_h);
+    }
+}
--- app/src/main/java/hashems/mobile_development_projects/MainActivity.java
+++ app/src/main/java/hashems/mobile_development_projects/MainActivity.java
@@ -1,7 +1,11 @@
 package hashems.mobile_development_projects;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // LinearLayout Vertical
+        TextView listViewV = (TextView) findViewById(R.id.Layout1);
+        listViewV.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListViewVActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // LinearLayout Horizontal
+        TextView listViewH = (TextView) findViewById(R.id.Layout2);
+        listViewH.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListViewHActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // GridLayout
+        TextView gridView = (TextView) findViewById(R.id.Layout3);
+        gridView.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GridViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // RelativeLayout
+        TextView relativeView = (TextView) findViewById(R.id.Layout4);
+        relativeView.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RelativeViewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
